7f18c9d57679347fbc7f08826bbd9af7fe043e0c
SpyrexDE <malikfk100@gmail.com>
2022-02-19 15:32:27 +0100

    Add line drawing

--- src/graphics/window.cpp
+++ src/graphics/window.cpp
@@ -1,16 +1,48 @@
 #include "window.h"
 
 namespace Velium::Graphics {
-    void windowResize(GLFWwindow* window, int width, int height);
+    void window_resize(GLFWwindow* window, int width, int height);
 
     Window::Window(const char *title, int height, int width)
             : m_title(title), m_height(height), m_width(width) {
         if (!init())
             glfwTerminate();
+
+        for (int i = 0; i < MAX_KEYS; i++)
+        {
+            m_keys[i] = false;
+        }
+
+        for (int i = 0; i < MAX_BUTTONS; i++)
+        {
+            m_mouse_buttons[i] = false;
+        }
     }
 
     Window::~Window() {
         glfwTerminate();
+    }
+
+    bool Window::isKeyPressed(unsigned int keycode) const
+    {
+        if (keycode >= MAX_KEYS)
+            return false;
+
+        return m_keys[keycode];
+    }
+
+    bool Window::isMouseButtonPressed(unsigned int button) const
+    {
+        if (button >= MAX_BUTTONS)
+            return false;
+
+        return m_mouse_buttons[button];
+    }
+
+    void Window::getMousePosition(double& x, double& y) const
+    {
+        x = m_mx;
+        y = m_my;
     }
 
     void Window::clear() const
@@ -25,9 +57,28 @@ namespace Velium::Graphics {
         glfwSwapBuffers(m_window);
     }
 
-    void windowResize(GLFWwindow* window, int width, int height)
+    void window_resize(GLFWwindow* window, int width, int height)
     {
         glViewport(0, 0, width, height);
+    }
+
+    void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+    {
+        Window* win = (Window*)glfwGetWindowUserPointer(window);
+        win->m_keys[key] = action != GLFW_RELEASE;
+    }
+
+    void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
+    {
+        Window* win = (Window*)glfwGetWindowUserPointer(window);
+        win->m_mouse_buttons[button] = action != GLFW_RELEASE;
+    }
+
+    void cursor_position_callback(GLFWwindow* window, double xpos, double ypos)
+    {
+        Window* win = (Window*)glfwGetWindowUserPointer(window);
+        win->m_mx = xpos;
+        win->m_my = ypos;
     }
 
     bool Window::init() {
@@ -44,7 +95,20 @@ namespace Velium::Graphics {
         }
 
         glfwMakeContextCurrent(m_window);
-        glfwSetWindowSizeCallback(m_window, windowResize);
+        glfwSetWindowSizeCallback(m_window, window_resize);
+
+        glfwSetWindowUserPointer(m_window, this);
+        glfwSetWindowSizeCallback(m_window, window_resize);
+        glfwSetKeyCallback(m_window, key_callback);
+        glfwSetMouseButtonCallback(m_window, mouse_button_callback);
+        glfwSetCursorPosCallback(m_window, cursor_position_callback);
+
+        if(glewInit() != GLEW_OK)
+        {
+            std::cout << "Failed to initialize GLEW" << std::endl;
+            return false;
+        }
+
         return true;
     }
 
--- src/graphics/window.h
+++ src/graphics/window.h
@@ -1,10 +1,14 @@
 #pragma once
-#include "GLFW/glfw3.h"
 #include "iostream"
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
+
+#define MAX_KEYS 1024
+#define MAX_BUTTONS 32
 
 namespace Velium::Graphics {
 
-    void windowResize(GLFWwindow* window, int width, int height);
+    void window_resize(GLFWwindow* window, int width, int height);
 
     class Window
     {
@@ -13,6 +17,11 @@ namespace Velium::Graphics {
         int m_width, m_height;
         GLFWwindow* m_window;
         bool m_closed;
+
+        bool m_keys[MAX_KEYS];
+        bool m_mouse_buttons[MAX_BUTTONS];
+        double m_mx, m_my;
+
     public:
         Window(const char* title, int height, int width);
         ~Window();
@@ -22,7 +31,15 @@ namespace Velium::Graphics {
 
         inline int getWidth() const { return m_width; }
         inline int getHeight() const { return m_height; }
+
+        bool isKeyPressed(unsigned int keycode) const;
+        bool isMouseButtonPressed(unsigned int button) const;
+        void getMousePosition(double& x, double& y) const;
     private:
         bool init();
+
+        friend void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+        friend void mouse_button_callback(GLFWwindow* window, int button, int action, int mods);
+        friend void cursor_position_callback(GLFWwindow* window, double xpos, double ypos);
     };
 }
