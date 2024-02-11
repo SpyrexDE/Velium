3e3f07985d492c83b23e6d9fadb94a071e5f263b
Spyrex <malikfk100@gmail.com>
2024-02-11 20:16:27 +0100

    Remove build files

--- build/Game/CMakeFiles/copy_game_resources.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/copy_game_resources"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/copy_game_resources.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
